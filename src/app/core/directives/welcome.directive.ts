bdb20b16b33e1fbdf63a0086e2aaeeb2350463d8
ySertacc <ysertac96@gmail.com>
2024-04-26 15:42:15 +0300

    Refactor welcome

--- src/app/core/directives/welcome.directive.ts
+++ src/app/core/directives/welcome.directive.ts
@@ -5,18 +5,28 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
   standalone: true,
 })
 export class WelcomeDirective {
-  @Input('appWelcome') set delay(time: number) {
+  /* @Input('appWelcome') set delay(time: number) {
     //this.viewContainer.clear();
     setTimeout(() => {
       document.getElementById('welcome')?.remove();
       this.viewContainer.createEmbeddedView(this.template);
     }, time);
   }
+*/
 
+  delay: number = 2000;
+  @Input('appWelcome') set outputDelay(value: TemplateRef<HTMLElement>) {
+    this.viewContainer.createEmbeddedView(this.template);
+    setTimeout(() => {
+      this.viewContainer.clear();
+      this.viewContainer.createEmbeddedView(value);
+    }, this.delay);
+  }
   constructor(
     private template: TemplateRef<HTMLElement>,
     private viewContainer: ViewContainerRef
   ) {}
 }
+
 //appWelcome isminde bir directive oluşturun. appWelcome directive ile verilen değer kadar ms değerinde gecikmeyle
 //içindeki elemanlarını yazdırınız. Gecikmeden önce Welcome yazsını yazdırınız. Bu şekilde bir structural directive yazınız.
